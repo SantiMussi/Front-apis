70ab72bd3c730e650d0417a026f72d62ba30ac05
Santiago Mussi <santimussi1959@gmail.com>
2025-10-07 20:09:34 -0300

    First commit

---
+++ FrontApis/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="bg-shapes">
+        <div className="shape"></div>
+        <div className="shape"></div>
+        <div className="shape"></div>
+      </div>
+
+      <div className="hero-content">
+        <h1>NUEVA COLECCIÓN</h1>
+        <p>OTOÑO / INVIERNO 2025</p>
+        <button className="cta-button">Explorar Ahora</button>
+      </div>
+    </section>
+  );
+}
---
+++ FrontApis/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="logo">SZAFRANKUS</div>
+
+      <ul className="nav-links">
+        <li><a href="#nueva">Nueva</a></li>
+        <li><a href="#mujer">Indumentaria</a></li>
+        <li><a href="#hombre">Arma tu outfit</a></li>
+        <li><a href="#colecciones">Colecciones</a></li>
+      </ul>
+
+      <ul className="nav-links right">
+        <li><a href="#carrito">Carrito</a></li>
+      </ul>
+    </nav>
+  );
+}
