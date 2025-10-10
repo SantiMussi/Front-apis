3b8ae05a50c495557cc20438be1fcdabdd646070
Nehuelin <nehuel2005@gmail.com>
2025-10-09 21:13:30 -0300

    + added routing to navbar

--- FrontApis/src/App.jsx
+++ FrontApis/src/App.jsx
@@ -1,13 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cart from "./views/Cart";
 import "./styles.css";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Hero />
-    </>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- FrontApis/src/components/Navbar.jsx
+++ FrontApis/src/components/Navbar.jsx
@@ -1,17 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav>
-      <div className="logo">SZAFRANKUS</div>
+      <div className="logo"><Link to="/">SZAFRANKUS</Link></div>
 
       <ul className="nav-links">
-        <li><a href="#nueva">Nueva</a></li>
-        <li><a href="#indumentaria">Indumentaria</a></li>
-        <li><a href="#hombre">Arma tu outfit</a></li>
-        <li><a href="#colecciones">Colecciones</a></li>
+        <li><Link to="/nueva">Nueva</Link></li>
+        <li><Link to="/indumentaria">Indumentaria</Link></li>
+        <li><Link to="/hombre">Arma tu outfit</Link></li>
+        <li><Link to="/colecciones">Colecciones</Link></li>
       </ul>
 
       <ul className="nav-links right">
-        <li><a href="#carrito">Carrito</a></li>
+        <li><Link to="/cart">Carrito</Link></li>
       </ul>
     </nav>
   );
