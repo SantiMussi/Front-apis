9605e94e7b1cbb5d11ae4908224546747c22806c
Nehuelin <nehuel2005@gmail.com>
2025-10-13 12:23:57 -0300

    Added comments

--- FrontApis/src/views/Cart.jsx
+++ FrontApis/src/views/Cart.jsx
@@ -1,9 +1,22 @@
 const Cart = () => {
     return (
         <>
-            <h1>Shopping Cart Poggies</h1>
+            <main className="cart-page">
+                 <h2>Carrito de Compras</h2>
+            </main>
         </>
     )
 }
 
 export default Cart
+
+
+/*Mostrar productos en el carrito
+
+
+Por cada producto queremos mostrar:
+--> Precio Unitario (descontado si aplica) 
+--> Cantidad 
+--> Precio Total 
+--> Boton para eliminar el producto del carrito
+*/
