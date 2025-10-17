bb0b7f67064369b8918fb9859a8395ec769a4358
Nehuelin <nehuel2005@gmail.com>
2025-10-17 16:24:33 -0300

    checkout planning

---
+++ FrontApis/src/views/CheckoutView.jsx
@@ -0,0 +1,19 @@
+const CheckoutView = () => {
+  return (
+    <>
+    
+    </>
+    );
+};
+
+export default CheckoutView;
+
+/* CHECKOUT
+- Tiene resumen con forma de envio, etc
+- Permite seleccionar metodo de pago
+- Aplicar cupon de descuento
+- Finalizar compra
+
+--> Una vez finalizada, muestra id de factura y resumen
+--> Tambien mensaje de "hemos enviado un email con los detalles"
+*/
