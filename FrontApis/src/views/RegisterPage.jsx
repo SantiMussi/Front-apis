1b3ddf624c6c42b624523998ad2010c80f58688d
juanma <juanmanuelguida@gmail.com>
2025-10-17 10:53:41 -0300

    Animation in register and login

--- FrontApis/src/views/RegisterPage.jsx
+++ FrontApis/src/views/RegisterPage.jsx
@@ -5,6 +5,11 @@ import "../components/Register/Register.css";
 const RegisterPage = () => {
   return (
     <div className="register-page">
+        <div className="bg-shapes">
+            <div className="shape"></div>
+            <div className="shape"></div>
+            <div className="shape"></div>
+        </div>
       <div className="register-box">
         <Register />
       </div>
