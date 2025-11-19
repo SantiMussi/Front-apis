ae4fccfe4165ddb265eeb98d5d286ee43abbbe3b
juanma <juanmanuelguida@gmail.com>
2025-11-19 10:40:43 -0300

    Order slice

--- FrontApis/src/redux/store.js
+++ FrontApis/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./productsSlice";
 import couponsReducer from "./couponsSlice";
 import categoriesReducer from "./categoriesSlice";
+import ordersReducer from "./ordersSlice.js"
 
 export const store = configureStore({
   reducer: {
     products: productsReducer, // registra el slice
     coupons: couponsReducer, 
     categories: categoriesReducer,
+    orders: ordersReducer,
   },
 });
 
